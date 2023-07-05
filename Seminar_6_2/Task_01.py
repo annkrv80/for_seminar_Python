b5a55c2a31a6cd223559b5d745a674616ab6d57d
Anna <annkrv80@yandex.ru>
2023-07-05 21:53:49 +0700

    Task from Seminar_6

---
+++ Seminar_6_2/Task_01.py
@@ -0,0 +1,28 @@
+from random import randint
+import sys
+
+START = 10
+STOP = 100
+chance = 10
+
+def lottery(start, stop, count):
+    count = 0
+    num = randint(start, stop)
+
+    while count < chance:
+        dig = int(input(f'Попытка №{chance - count}. Отгадай число: '))
+        count += 1
+        if dig > num:
+            print('Меньше')
+        elif dig < num:
+            print('Больше')
+        elif dig == num:
+            print('ПОБЕДА!!!')
+            return True
+        if count == chance:
+            print(f'Увы! Загаданое число{num}')
+            return False
+
+if __name__ == '__main__':
+    _, START, STOP, CHANGE = sys.argv
+    lottery(int(START), int(STOP), int(CHANGE))
